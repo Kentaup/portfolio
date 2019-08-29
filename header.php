3e66ec6c63d5d47c2c848ba808773dc222f6aa75
Kentaup <kenta.mori.scriptg@gmail.com>
2019-08-29 18:31:08 +0900

    test

--- header.php
+++ header.php
@@ -2,7 +2,9 @@
 
     <tr>
       <th>MENU</th>
-      <th ><a href="creation" class="mgl">MyCreation</a></th>
+      <th ><a href="index.php" class="mgl">HOME</a></th>
+      &emsp;
+      <th ><a href="creation.php" class="mgl">MyCreation</a></th>
       &emsp;
       <th><a href="profile.php">MyProfile</a></th>
     </tr>
