dcf47406dd0efa3767a7550010bc3a80141725d1
Kentaup <kenta.mori.scriptg@gmail.com>
2019-08-29 14:37:55 +0900

    first

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+
+    <tr>
+      <th>MENU</th>
+      <th ><a href="creation" class="mgl">MyCreation</a></th>
+      &emsp;
+      <th><a href="profile.php">MyProfile  </a></th>
+    </tr>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>MyPortfolio</title>
+
+  </head>
+  <header>
+    <?php include(dirname(__FILE__).'/header.php'); ?>
+  </header>
+  <body>
+    <div class="content-bg">
+      <div class="content"></div>
+
+        <div class="title">
+          <p>MyPortfolio</p>
+        </div>
+
+          <p>content</p>
+
+      </div>
+    </div>
+
+  </body>
+  <footer><?php include(dirname(__FILE__).'/footer.php'); ?></footer>
+</html>
