690890ab930ad8c6cbe78d2e2096d3554fbc478c
Kentaup <kenta.mori.scriptg@gmail.com>
2019-08-29 17:38:14 +0900

    color

--- index.php
+++ index.php
@@ -8,13 +8,11 @@
   </head>
   <header><?php include(dirname(__FILE__).'/header.php'); ?></header>
   <body>
-    <div class="content-bg">
-      <div class="content">
+    <div class="back">
         <div class="title">
           <p>MyPortfolio</p>
         </div>
         <p>content</p>
-      </div>
     </div>
   </body>
   <footer><?php include(dirname(__FILE__).'/footer.php'); ?></footer>
