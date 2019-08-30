f6a14b9afcfb573589ec9d6d331fa8f1ffbc0fea
Kentaup <kenta.mori.scriptg@gmail.com>
2019-08-30 19:58:36 +0900

    icon

--- footer.php
+++ footer.php
@@ -3,8 +3,8 @@
   工学院大学情報学部コンピュータ科学科 <br>
   MAIL : j117277[a]ns.kogakuin.ac.jp</p>
 <a href="#">
-  <img src="icon_twitter" alt="">
+  <img src="images/icon_twitter.png" alt="">
 </a>
 <a href="#">
-  <img src="icon_facebook" alt="">
+  <img src="images/icon_facebook.png" alt="">
 </a>
