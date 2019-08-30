85f76546a9645943b0e25ec99206acdaaa539eaa
Kentaup <kenta.mori.scriptg@gmail.com>
2019-08-30 19:56:21 +0900

    footer

--- footer.php
+++ footer.php
@@ -2,3 +2,9 @@
 <p>森健太<br>
   工学院大学情報学部コンピュータ科学科 <br>
   MAIL : j117277[a]ns.kogakuin.ac.jp</p>
+<a href="#">
+  <img src="icon_twitter" alt="">
+</a>
+<a href="#">
+  <img src="icon_facebook" alt="">
+</a>
