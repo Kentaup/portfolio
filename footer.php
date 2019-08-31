7ce0033bb026e9773573858e6aca7dc92a215326
Kentaup <kenta.mori.scriptg@gmail.com>
2019-08-31 18:00:54 +0900

    footer

--- footer.php
+++ footer.php
@@ -3,8 +3,8 @@
   工学院大学情報学部コンピュータ科学科 <br>
   MAIL : j117277[a]ns.kogakuin.ac.jp</p>
 <a href="#">
-  <img src="images/icon_twitter.png" alt="">
+  <img src="images/icon_twitter.png" height="70px" alt="">
 </a>
 <a href="#">
-  <img src="images/icon_facebook.png" alt="">
+  <img src="images/icon_facebook.png" height="70px" alt="">
 </a>
--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <div class="title">
           <p>Kenta Mori Portfolio</p>
         </div>
-        <div class="content-outer" id="profile" >
+        <div class="content-outer" id="profile">
           <div class="content">
             <img class="icon" src="images/icon_profile.png" alt="">
             <h1>Profile</h1>
@@ -21,8 +21,8 @@
             <h3>工学院大学 情報学部 コンピュータ科学科</h3>
           </div>
         </div>
-        <div class="content-outer">
-          <div class="content" id="works" >
+        <div class="content-outer" id="works">
+          <div class="content" >
             <img class="icon" src="images/icon_works.png" alt="">
             <h1>Works</h1>
             <h3>myApp</h3>
