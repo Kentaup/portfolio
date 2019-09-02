d7e147aad999918a764cdacd7fe9506cdd56766e
Kentaup <kenta.mori.scriptg@gmail.com>
2019-09-02 17:06:56 +0900

    link

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
 <p>森健太<br>
   工学院大学情報学部コンピュータ科学科 <br>
   MAIL : j117277[a]ns.kogakuin.ac.jp</p>
-<a href="#">
+<a href="https://twitter.com/moridaiko">
   <img src="images/icon_twitter.png" alt="">
 </a>
-<a href="#">
+<a href="https://www.facebook.com/profile.php?id=100040431405771">
   <img src="images/icon_facebook.png" alt="">
 </a>
