d38f429bcf147eeba5988ffaf5b493195d777fc2
Kentaup <kenta.mori.scriptg@gmail.com>
2019-10-04 22:16:49 +0900

    in

--- index.php
+++ index.php
@@ -13,7 +13,24 @@
     <header><?php include(dirname(__FILE__).'/header.php'); ?></header>
 
     <div class="containar">
-      
+      <h1>ポートフォリオ</h1>
+      <div class="row border border-dark">
+        <h1>プロフィール</h1>
+        <p>森健太 20歳</p>
+        <p>工学院大学情報学部コンピュータ科学科所属</p>
+        <p>経験</p>
+        <ul>
+          <li>大学の講義でC,Javaを修得</li>
+          <li>大学の委員会でHPの運営を担当(<a href="http://www.ns.kogakuin.ac.jp/~wws5018/">工学院大学学生団体ポータルサイトSHAiR</a>)</li>
+          <li>株式会社セレスの３日間インターンにてPHPを用いたwebアプリのチーム開発を経験</li>
+          <li>現在Rubyを習得中</li>
+        </ul>
+        <p>趣味:和太鼓</p>
+      </div>
+      <div class="row border border-dark">
+        <h1>成果物</h1>
+        <p>和太鼓の会 鼓遊 ホームページ</p>
+      </div>
     </div>
 
     <footer><?php include(dirname(__FILE__).'/footer.php'); ?></footer>
