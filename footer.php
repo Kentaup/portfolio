993373ff5085d9bc21bf15a884e44baa821192b5
Kentaup <kenta.mori.scriptg@gmail.com>
2019-10-04 21:48:48 +0900

    bg

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<div class="containar bg-dark text-white">
+<div class="containar bg-secondary text-white">
   <div class="row">
     <div class="col-6 ml-3">
       <p>NAME : 森健太 MoriKenta<br>
         UNIVERSITY : 工学院大学情報学部コンピュータ科学科 <br>
         MAIL : morikenta.se@gmail.com</p>
     </div>
-    <div class="col-6">
+    <div class="col-6 mt-3">
       <a href="https://twitter.com/moridaiko" target="_blank">
         <img src="images/icon_twitter.png" alt="">
       </a>
