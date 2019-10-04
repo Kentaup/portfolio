a8221eb934e55fab2d95704d59aed63a693eaa01
Kentaup <kenta.mori.scriptg@gmail.com>
2019-10-04 21:41:12 +0900

    col

--- footer.php
+++ footer.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <div class="containar bg-dark">
-  <p>NAME : 森健太 MoriKenta<br>
-    UNIVERSITY : 工学院大学情報学部コンピュータ科学科 <br>
-    MAIL : morikenta.se@gmail.com</p>
-  <a href="https://twitter.com/moridaiko" target="_blank">
-    <img src="images/icon_twitter.png" alt="">
-  </a>
-  <a href="https://www.facebook.com/profile.php?id=100040431405771" target="_blank">
-    <img src="images/icon_facebook.png" alt="">
-  </a>
+  <div class="row">
+    <div class="col-6">
+      <p>NAME : 森健太 MoriKenta<br>
+        UNIVERSITY : 工学院大学情報学部コンピュータ科学科 <br>
+        MAIL : morikenta.se@gmail.com</p>
+    </div>
+    <div class="col-6">
+      <a href="https://twitter.com/moridaiko" target="_blank">
+        <img src="images/icon_twitter.png" alt="">
+      </a>
+      <a href="https://www.facebook.com/profile.php?id=100040431405771" target="_blank">
+        <img src="images/icon_facebook.png" alt="">
+      </a>
+    </div>
+  </div>
 </div>
