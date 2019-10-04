cc6d0f7d4e38d6c5ab247149034915e905f98278
Kentaup <kenta.mori.scriptg@gmail.com>
2019-10-04 21:09:49 +0900

    fd

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<div class="containar bg-danger">
+<div class="containar bg-dark">
   <p>NAME : 森健太 MoriKenta<br>
     UNIVERSITY : 工学院大学情報学部コンピュータ科学科 <br>
     MAIL : j117277[a]ns.kogakuin.ac.jp</p>
