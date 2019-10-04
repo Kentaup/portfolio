ef827f62f55cb1ee575284605f0337e609843fec
Kentaup <kenta.mori.scriptg@gmail.com>
2019-10-04 19:42:19 +0900

    bootf

--- header.php
+++ header.php
@@ -9,9 +9,40 @@
       <th><a href="profile.php">MyProfile</a></th>
     </tr> -->
 
-    <div class="menu-box">
+    <!-- <div class="menu-box">
       <a href="#top">TOP</a>
       <a href="#profile">Profile</a>
       <a href="#works">Works</a>
       <a href="#skills">Skills</a>
-    </div>
+    </div> -->
+
+    <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-hd">
+        <a href="https://koyu.herokuapp.com">
+          <img class="navbar-brand" src="images/koyu_logo.png" height="50px" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="navbarNav4" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id=menu>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" id="home" href="https://koyu.herokuapp.com"><h4>ホーム</h4> </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="info" href="info.php"><h4>お知らせ</h4></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="about" href="about.php"><h4>鼓遊について</h4></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="member" href="member.php"><h4>団員紹介</h4></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="blog" href="http://koyu-taiko.blogspot.com" target="_blank"><h4>ブログ</h4></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="contact" href="contact.php"><h4>お問い合わせ</h4></a>
+            </li>
+          </ul>
+        </div>
+    </nav>
