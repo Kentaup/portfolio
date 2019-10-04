714a4bc027f4a70f41608b9ddf1fa3c53f03be77
Kentaup <kenta.mori.scriptg@gmail.com>
2019-10-04 22:59:32 +0900

    li

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <h2 class="">プロフィール</h2>
         <p class="col-sm-12">森健太 20歳</p>
         <p class="col-sm-12">工学院大学情報学部コンピュータ科学科3年  <a href="https://www.kogakuin.ac.jp/faculty/lab/info_lab145.html" target="_blank">高信頼ソフトウェア開発工学研究室</a> 所属</p>
-        <p class="col-sm-12">経験</p>
+        <p>経験</p>
         <ul>
           <li>大学の講義でC,Javaを修得</li>
           <li>大学の委員会でHPの運営を担当(<a href="http://www.ns.kogakuin.ac.jp/~wws5018/" target="_blank">工学院大学学生団体ポータルサイトSHAiR</a>)</li>
@@ -30,7 +30,17 @@
         <h2 class="col-sm-12">成果物</h2>
         <p class="col-sm-12">和太鼓の会 鼓遊 ホームページ</p>
       </div>
+      <div class="row border border-dark mt-3">
+        <h2 class="col-sm-12">リンク</h2>
+        <ul>
+          <li>GitHub : </li>
+          <li>Twitter : </li>
+          <li>facebook : </li>
+          <!-- <li>Qiita : </li> -->
+        </ul>
+      </div>
     </div>
+
 
     <footer><?php include(dirname(__FILE__).'/footer.php'); ?></footer>
   </body>
