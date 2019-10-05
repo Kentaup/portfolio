94bf36144740a9107275232eb67e25d8784b1294
Kentaup <kenta.mori.scriptg@gmail.com>
2019-10-06 08:24:31 +0900

    liu

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
         <h2 class="col-sm-12">プロフィール</h2>
         <p class="col-sm-12">森健太 20歳</p>
         <p class="col-sm-12">工学院大学情報学部コンピュータ科学科3年  <a href="https://www.kogakuin.ac.jp/faculty/lab/info_lab145.html" target="_blank">高信頼ソフトウェア開発工学研究室</a> 所属</p>
-        <p class="col-sm-12">経験</p>
         <ul>
+          <p class="col-sm-12">経験</p>
           <li>大学の講義でC,Javaを修得</li>
           <li>大学の委員会でHPの運営を担当(<a href="http://www.ns.kogakuin.ac.jp/~wws5018/" target="_blank">工学院大学学生団体ポータルサイトSHAiR</a>)</li>
           <li>株式会社セレスの３日間インターンにてPHPを用いたwebアプリのチーム開発を経験</li>
