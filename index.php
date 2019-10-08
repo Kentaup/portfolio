f710357ffa624c35348be3a4cafc908ef30e82cc
Kentaup <kenta.mori.scriptg@gmail.com>
2019-10-08 16:26:57 +0900

    size

--- index.php
+++ index.php
@@ -31,10 +31,10 @@
       <div class="row border border-dark m-3">
         <h2 class="col-sm-12">成果物</h2>
         <div class="row m-3">
-          <a class="col-md-3 col-12" href="https://koyu.herokuapp.com/" target="_blank">
+          <a class="col-lg-3 col-12" href="https://koyu.herokuapp.com/" target="_blank">
             <img  src="images/koyu_thumbnail.png" alt="和太鼓の会 鼓遊 ホームページ">
           </a>
-          <div class="col-md-9 col-12">
+          <div class="col-lg-9 col-12">
             <h5 class="col-sm-12"><a href="https://koyu.herokuapp.com/" target="_blank">和太鼓の会 鼓遊 ホームページ</a></h5>
             <p class="col-sm-12">使用技術:HTML,CSS,Bootstrap4,PHP,PostgreSQL</p>
             <p class="col-sm-12">私の所属する和太鼓チームのホームページです。チームの広報を強化する目的で作成しました。</p>
